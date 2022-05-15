b45f1277c2791db985873053975030d764cb3f81
virux <virux.flash@gmail.com>
2022-05-15 07:14:35 +0400

    polygon

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,7 +60,7 @@ const TOKENS_PER_MINT = 100; // maximum number of NFTs a user can mint in a sing
 const PUBLIC_MINT_START_DATE = "2022-05-15T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-11T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x2be877F5e59Ad96e145F0165e4AeBeE4C96e7340"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
